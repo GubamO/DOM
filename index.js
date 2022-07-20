946a49f4a148e2092a699b67304337eb7c6748eb
baudson <guibau2303@gmail.com>
2022-07-20 11:52:59 +0200

    fin début dom

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-const b = document.body;
+// ex 1 DOM - fixed background color 
 
-b.style.backgroundColor = "yellow"; 
+function changeBackground(color) {
+    document.body.style.background = color;
+ }
+ 
+ window.addEventListener("load",function() { changeBackground('#FF69B4') });
+
+ // random colors
+
+function randomColorHex()
+{
+    let rndRed = parseInt(Math.random() * 256);
+    let rndGreen = parseInt(Math.random() * 256);
+    let rndBlue = parseInt(Math.random() * 256);
+    return `#${rndRed.toString(16)}${rndGreen.toString(16)}${rndBlue.toString(16)}`;
+}
+document.body.style.backgroundColor = randomColorHex();
+
+// display all children elements 
+
+ for(let i = 0; i < document.body.children.length; i++)
+ {
+     if(i == 1)
+     {
+         for(let child of document.body.children[i].childNodes)
+         {
+             console.log(child);
+         }
+         break;
+     }
+ }
